f654ae438b71535f24f3be0bb8e0e8bd12038dbf
Diogo Matos <diogomatos@Diogos-MacBook-Pro.local>
2023-05-02 13:27:59 +0100

    Sql query for creation of questions

--- src/Controller/questions.java
+++ src/Controller/questions.java
@@ -4,12 +4,13 @@ package Controller;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import java.util.Hashtable;
 import java.util.List;
 
 public class questions {
 
-    public static void createQuestion(String Description, String Difficulty, String Type, List<String> answer, String Subject, String theme) throws SQLException {
+    public static void createQuestion(String Description, String Difficulty, String Type, Hashtable<String, List> answer, String Subject, String theme) throws SQLException {
         // Por agora lista mas poderá ser modificado
         // Descrição do q acontece aqui
         // TODOS OS PARAMETROS TEM DE PASSAR NUM CONTROLO ANTES DE VIREM PARA ESTE METHOD.
@@ -30,43 +31,50 @@ public class questions {
         connection.execute_query(conn, subject_query);
         
         switch(Type){
-            case "Escolha Múltipla":
-                // VER COMO o parametro answer estara configurado para tratar os dados.
-
-                connection.execute_query(conn, String.format("INSERT INTO MULTIPLECHOICE VALUES ('Periodo da Realeza', FALSE, FALSE, '2');"));
-                // INSERT INTO MULTIPLECHOICE VALUES ('Periodo da Realeza', FALSE, FALSE, '2');
-                // INSERT INTO MULTIPLECHOICE VALUES ('Periodo da Democracia', FALSE, TRUE, '2');
-                // INSERT INTO MULTIPLECHOICE VALUES ('Periodo do Principado', FALSE, FALSE, '2');
-                // INSERT INTO MULTIPLECHOICE VALUES ('Periodo do Baixo Império', FALSE, FALSE, '2');
-
+            case "Escolha Múltipla" :
             case "Resposta Múltipla":
-                // VER COMO o parametro answer estara configurado para tratar os dados.
+                Integer n_answerM = Integer.parseInt((String) answer.get("n_answer").get(0));
 
-                // INSERT INTO MULTIPLECHOICE VALUES ('Modela o fluxo de tempo dentro de um sistema', TRUE, FALSE, '6');
-                // INSERT INTO MULTIPLECHOICE VALUES ('Inclui atores e casos de uso', TRUE, TRUE, '6');
-                // INSERT INTO MULTIPLECHOICE VALUES ('Especifica a decomposição funcional de um sistema', TRUE, FALSE, '6');
-                // INSERT INTO MULTIPLECHOICE VALUES ('Especifica quem realiza quais tarefas no sistema que está a ser desenvolvido', TRUE, TRUE, '6');
+                for(int i = 0; i < n_answerM; i++){
+                    List<String> row = answer.get(i);
+                    String choice = row.get(0); // question's answer
+                    Boolean val = Boolean.parseBoolean(row.get(1)); // Bool to check if it is "Escolha múltipla" or "Resposta Múltipla "
+                    Boolean torf = Boolean.parseBoolean(row.get(2)); // Bool to check if a answer is true or false.
+                    
+
+                    connection.execute_query(conn, String.format("INSERT INTO MULTIPLECHOICE VALUES((%s, %b, %b, %s);", choice, val, torf, id));
+                }
+                break;
 
             case "Desenvolvimento":
-                // VER COMO o parametro answer estara configurado para tratar os dados.
+                String row_d = (String) answer.get("answer").get(0);
+                connection.execute_query(conn, String.format("INSERT INTO DEVELOPMENT VALUES (%s, %s);", row_d, id));
 
-                //INSERT INTO DEVELOPMENT VALUES ('As empresas cotadas possuem maiores exigências sobre a normalização contabilísticas, assim é obrigada a ter todos os tipos de demonstrações financeiras. Quanto menor a empresa, menor será a sua exigência sobre normalização contabilística, ou seja, também serão necessárias menos demonstrações financeiras.', '3' );
-
+                break;
+        
             case "Resposta Curta":
-                // VER COMO o parametro answer estara configurado para tratar os dados.
+                String row_c = (String) answer.get("answer").get(0);
+                connection.execute_query(conn, String.format("INSERT INTO SHORT_ANSWER VALUES (%s, %s);", row_c, id));
 
-                // INSERT INTO SHORT_ANSWER VALUES ("Opera com pouca ou nenhuma assistência, oferece suporte a decisões e problemas", "1");
-            
+                break;
+    
+
             case "Correspondência de Colunas":
-                // VER COMO o parametro answer estara configurado para tratar os dados.
+                Integer n_answerC = Integer.parseInt((String) answer.get("n_answer").get(0));
 
-                // INSERT INTO MATCHFOLLOWING VALUES ('Verificação e recuperação de erros e proteção contra falhas do sistema', 'Requisitos funcionais', '5');
-                // INSERT INTO MATCHFOLLOWING VALUES ('Estados e condições que não devem acontecer', 'Requisitos de exclusão', '5');
-                // INSERT INTO MATCHFOLLOWING VALUES ('Fiabilidade e disponibilidade desejadas para o sistema', 'Requisitos não funcionais', '5');
+                for(int i = 0; i < n_answerC; i++){
+                    List<String> row = answer.get(i);
+                    String left = row.get(0);
+                    String right = row.get(1);
+                    connection.execute_query(conn, String.format("INSERT INTO MATCHFOLLOWING VALUES (%s, %s, %s);",left, right, id));
+                }
+
+                break;
 
             case "Resposta Calculada":
                 // VER COMO o parametro answer estara configurado para tratar os dados.
                 //Duvida
+                // FALAR COM O PROF
 
                 //INSERT INTO CALCULATEDQ VALUES ('80640', '4'); 
 
